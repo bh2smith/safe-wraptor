c4bb0830b142629cf316381d4f60b89ea01df817
David Sato <david.sato64@gmail.com>
2020-12-21 13:56:40 +0100

    stop infini-loop

---
+++ src/hooks/index.ts
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import { initWeb3 } from 'src/connect'
+import Web3 from 'web3'
+
+const useWindowLoad = () => {
+    const [windowLoaded, setWindowLoaded] = useState(false)
+  
+    useEffect((): void | (() => void) => {
+      if (!window || typeof window.addEventListener !== 'function') return
+      
+      function listener() {
+        return setWindowLoaded(true)
+      }
+  
+      window.addEventListener('load', listener)
+    
+      return () => window.removeEventListener('load', listener)
+    }, [])
+  
+    return windowLoaded
+  }
+  
+  interface BlockchainParams {
+    network?: string
+  }
+  
+  export const useActiveWeb3 = ({ network }: BlockchainParams) => {
+    const [web3, setWeb3] = useState<Web3>(null)
+    const windowLoaded = useWindowLoad()
+  
+    useEffect(() => {
+      if (!windowLoaded || !network) return
+  
+      const web3 = initWeb3(network)
+  
+      setWeb3(web3)
+    }, [network, windowLoaded])
+    return web3
+  }
+  
+  export const useBlockNumber = ({ network }: BlockchainParams) => {
+    const [blockNumber, setBlockNumber] = useState(0)
+    const web3 = useActiveWeb3({ network })
+  
+    useEffect((): void | (() => void) => {
+      function handler(e, r) {
+        if (e) throw new Error(e)
+  
+        setBlockNumber(r.number)
+      }
+  
+      if (!web3) return
+  
+      const sub = web3.eth.subscribe('newBlockHeaders', handler)
+  
+      return () => sub.unsubscribe()
+    }, [web3])
+  
+    return blockNumber
+  }
